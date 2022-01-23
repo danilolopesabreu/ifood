fa31bcc585633d15388bdd811b1f054f935eb604
danilolopesabreu <danilo.bcc@gmail.com>
2022-01-23 12:16:54 -0300

    start structure

---
+++ register/src/test/java/com/github/danilolopesabreu/ifood/register/NativeHelloResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.danilolopesabreu.ifood.register;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
